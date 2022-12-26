2e7b1a433674323252318f24e994eae16603b157
d_van <117540976+dinhvan2@users.noreply.github.com>
2022-12-26 08:02:09 +0700

    lab4

---
+++ Core/Inc/display_7seg.h
@@ -0,0 +1,13 @@
+/*
+ * display_7seg.h
+ *
+ *  Created on: Nov 2, 2022
+ *      Author: Phạm Đình Văn
+ */
+
+#ifndef INC_DISPLAY_7SEG_H_
+#define INC_DISPLAY_7SEG_H_
+
+void show7SEG();
+
+#endif /* INC_DISPLAY_7SEG_H_ */
